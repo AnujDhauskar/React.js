05ec0fc17f669baab5a5da875a14e326e313e7bd
Anuj Dhauskar <anujdhauskar64@gmail.com>
2026-02-20 21:01:13 +0530

    Basics of react.js

---
+++ UI design project/src/components/section1/Section1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full bg-gray-950'> Section1</div>
+    
+  )
+}
+
+export default Section1
---
+++ useState/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const App = () => {
+const [num, setNum] = useState(0)
+  return (
+    <div>
+    <h1> {num}</h1>
+    <button onClick={()=>{
+      setNum  (num + 1)
+    }}> Increase </button>
+
+    <button onClick={()=>{
+      setNum  (num - 1)
+    }}> Decrease </button>
+    </div>
+  )
+}
+
+export default App
