56a03f821ec00909ec6cf76024e8c288d946fbce
Anuj Dhauskar <anujdhauskar64@gmail.com>
2026-08-12 13:43:23 +0530

    feat: initialize React project with Tailwind CSS and SVG icons
    - Added SVG icons for various applications (bluesky, discord, documentation, github, social, x).
    - Created App component with Tailwind CSS styling.
    - Imported Tailwind CSS in App.css.
    - Added main entry point (main.jsx) for React application.
    - Configured Vite with React and Tailwind CSS plugins.
    - Included assets: hero image, react logo, and vite logo.

---
+++ React_color/changing_color/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="bg-amber-950 text-white  ">hello world </h1>
+      <div class="m-10 p-10 bg-red-500 text-white font-bold text-3xl rounded-xl shadow-2xl">
+  If you can see this with a red background, Tailwind is working!
+</div>
+    </>
+  )
+}
+
+export default App
